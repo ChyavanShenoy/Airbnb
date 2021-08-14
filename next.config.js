d4d13c8b1c44857ac178d6f80482c418fe29e34c
Chyavan Shenoy <chyavandshenoy2318@gmail.com>
2021-08-14 14:16:10 +0530

    Mapbox additions

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1Ijoicm9va2llbWFzdGVyNjkiLCJhIjoiY2tzOGs4N2VpMGhwdTMwbHV2bmozY2x5dCJ9.ZUm0Kc6r0lDKiCT2gctUZg",
+  },
 };
